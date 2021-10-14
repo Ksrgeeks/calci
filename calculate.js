a51eae9fb95c816987db608cf8e8c19b14412b03
Kuldeep <51863998+Ksrgeeks@users.noreply.github.com>
2021-10-14 23:18:42 +0530

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $(".addition").keyup(function(){
+          var val1 = +$(".value1").val();
+          var val2 = +$(".value2").val();
+          $("#result").val(val1+val2);
+   });
+});
+
+$(document).ready(function(){
+    $(".subtract").keyup(function(){
+          var val1 = +$(".value1").val();
+          var val2 = +$(".value2").val();
+          $("#result1").val(val1-val2);
+   });
+});
+
+$(document).ready(function(){
+    $(".multiply").keyup(function(){
+          var val1 = +$(".value1").val();
+          var val2 = +$(".value2").val();
+          $("#result2").val(val1*val2);
+   });
+});
+
+$(document).ready(function(){
+    $(".division").keyup(function(){
+          var val1 = +$(".value1").val();
+          var val2 = +$(".value2").val();
+          $("#result3").val(val1/val2);
+   });
+});
+
+$(document).ready(function(){
+    $(".remainder").keyup(function(){
+          var val1 = +$(".value1").val();
+          var val2 = +$(".value2").val();
+          $("#result4").val(val1%val2);
+   });
+});
+
+$(document).ready(function(){
+    $(".percentage").keyup(function(){
+          var val1 = +$(".value1").val();
+          var val2 = +$(".value2").val();
+          var val3 = +$(".value3").val();
+          var val4 = +$(".value4").val();
+          $("#result5").val(val2*val1/100);
+          $("#result6").val(val4*100/val3);
+   });
+});
+
